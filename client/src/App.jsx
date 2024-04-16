5ab3147f71833c2599e727a206b28b64844779ec
GusalsTheNoob <61675516+Hyeonmin-Kim@users.noreply.github.com>
2024-04-16 12:18:38 -0700

    ADD baseline header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,15 @@
-// import { useState } from 'react'
-import axios from 'axios'
 import './App.css'
-import Button from '@mui/material/Button';
-
-//data will be the string we send from our server
-const apiCall = () => {
-  axios.get('http://localhost:3000').then((data) => {
-    //this console.log will be in our frontend console
-    console.log(data)
-  })
-}
+import Header from './assets/header';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <Button variant="contained" onClick={apiCall}>Make API Call</Button>
-
-      </header>
-    </div>
+    <>
+      <CssBaseline/>
+      <div>
+        <Header/>
+      </div>
+    </>
   );
 }
 
