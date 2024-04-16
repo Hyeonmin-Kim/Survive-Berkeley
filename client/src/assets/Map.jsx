7170638cf7efcb1c00af729109cac931a781dca3
GusalsTheNoob <61675516+Hyeonmin-Kim@users.noreply.github.com>
2024-04-16 14:31:59 -0700

    FIX wrong map initialization

--- client/src/assets/Map.jsx
+++ client/src/assets/Map.jsx
@@ -1,21 +1,25 @@
+import { useEffect } from 'react';
 import MAPBOX_KEY from "./credentials";
 import Box from '@mui/material/Box';
 
 const Map = () => {
-    const loadMap = (event) => {
+
+    useEffect(() => {
         mapboxgl.accessToken = MAPBOX_KEY;
         var map = new mapboxgl.Map({
             container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11'
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-122.259094, 37.871960],
+            zoom: 14
         });
-    }
+    })
 
     return (
         <div>
             <Box id="map" sx={{
                 width: "100%",
                 height: "calc(100vh - 64px)",
-            }} onClick={loadMap}></Box>
+            }}></Box>
         </div> 
     );
 }
