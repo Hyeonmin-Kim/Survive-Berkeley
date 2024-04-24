5b29aefd619a4deb4bb5d6ec3cea47d4c6cb5412
GusalsTheNoob <61675516+Hyeonmin-Kim@users.noreply.github.com>
2024-04-24 12:33:27 -0700

    FEAT popup function

--- client/src/assets/MainMap.jsx
+++ client/src/assets/MainMap.jsx
@@ -1,11 +1,37 @@
-import Map from 'react-map-gl';
+import { Map, Marker }  from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MAPBOX_API from './credentials';
 import { useRef } from 'react';
 import mainMapConfig from './mainMapConfig';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
 
-const MainMap = ({ hooker }) => {
+const MainMap = ({ hooker, popupHandler, highlightPins, highlightPinHandler }) => {
     const mainMapRef = useRef();
+
+    const getAddress = async (lng, lat) => {
+        const url = `https://api.mapbox.com/search/geocode/v6/reverse?longitude=${lng}&latitude=${lat}&access_token=${MAPBOX_API}`;
+        const address = await fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                return data.features[0].properties.name;
+            })
+            .catch(error => { 
+                console.log(error);
+                return "[UNSPECIFIED]";
+            });
+        return address;
+    } 
+
+    const mainMapOnClick = async (event) => {
+        const newPin = {
+            key: 0,
+            lng: event.lngLat.lng,
+            lat: event.lngLat.lat,
+        }
+        newPin.address = await getAddress(event.lngLat.lng, event.lngLat.lat);
+        highlightPinHandler([newPin]);
+        popupHandler(true);
+    };
 
     return (
         <Map
@@ -26,7 +52,13 @@ const MainMap = ({ hooker }) => {
                 console.log("onLoad");
                 hooker(mainMapRef);
             }}
+            onClick={mainMapOnClick}
         >
+            {highlightPins.map((highlightPin) => 
+                <Marker key={highlightPin.key} longitude={highlightPin.lng} latitude={highlightPin.lat} anchor="bottom" >
+                    <AddLocationIcon fontSize='large' color='primary'/>
+                </Marker>
+            )}
         </Map>
     );
 }
